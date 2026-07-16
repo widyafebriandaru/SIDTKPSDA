0bcda60378a6367e39a950b4524402e03f727a81
widyafebriandaru <febriandaru77@gmail.com>
2026-07-16 08:35:12 +0000

    fix init.py

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,3 +1,7 @@
-from app.crud import instansi, tusi, rekomendasi, tindak_lanjut, program_kerja
+from app.crud.instansi import instansi
+from app.crud.tusi import tusi
+from app.crud.rekomendasi import rekomendasi
+from app.crud.tindak_lanjut import tindak_lanjut
+from app.crud.program_kerja import program_kerja
 
 __all__ = ["instansi", "tusi", "rekomendasi", "tindak_lanjut", "program_kerja"]
